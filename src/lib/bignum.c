32d22d8efbb79fbfa4ffcbb618b73161f65c7eba
charliesome <charlie@charliesomerville.com>
2012-07-11 23:49:21 +1000

    placeholder bignum, float, int functions

--- src/lib/bignum.c
+++ src/lib/bignum.c
@@ -1,9 +1,34 @@
+#include <stdlib.h>
 #include "class.h"
 #include "value.h"
 
+static sl_object_t*
+allocate_bignum()
+{
+    return NULL;
+}
+
+void
+sl_init_bignum(sl_vm_t* vm)
+{
+    vm->lib.Bignum = sl_define_class(vm, "Bignum", vm->lib.Number);
+    sl_class_set_allocator(vm, vm->lib.Bignum, allocate_bignum);
+}
+
 SLVAL
-sl_make_bignum(int n)
+sl_make_bignum(sl_vm_t* vm, int n)
+{
+    (void)vm;
+    (void)n;
+    /* @TODO */
+    abort();
+}
+
+SLVAL
+sl_bignum_add(sl_vm_t* vm, SLVAL self, SLVAL other)
 {
     /* @TODO */
     abort();
+    self = other;
+    return vm->lib.nil;
 }
