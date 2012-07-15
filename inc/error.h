46a371dea4729223666ebcd79bb45c2b8ce2895b
charliesome <charlie@charliesomerville.com>
2012-07-15 21:43:36 +1000

    SL_TRY macro

--- inc/error.h
+++ inc/error.h
@@ -32,4 +32,18 @@ sl_throw_message(struct sl_vm* vm, char* cstr);
 void
 sl_throw_message2(struct sl_vm* vm, SLVAL klass, char* cstr);
 
+#define SL_TRY(frame, try_block, e, catch_block) do { \
+        frame.error = vm->lib.nil; \
+        frame.prev = vm->catch_stack; \
+        frame.error = vm->lib.nil; \
+        if(!setjmp(frame.env)) { \
+            try_block; \
+            vm->catch_stack = frame.prev; \
+        } else { \
+            vm->catch_stack = frame.prev; \
+            e = frame.error; \
+            catch_block; \
+        } \
+    } while(0)
+
 #endif
