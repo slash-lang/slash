7cb240d2578af1083109036d47ca736e82f4ebae
charliesome <charlie@charliesomerville.com>
2012-07-15 21:18:16 +1000

    allocate correct sized region for string

--- src/string.c
+++ src/string.c
@@ -84,7 +84,7 @@ sl_cstring(struct sl_vm* vm, char* cstr)
 static sl_object_t*
 allocate_string()
 {
-    sl_object_t* obj = (sl_object_t*)GC_MALLOC(sizeof(sl_object_t));
+    sl_object_t* obj = (sl_object_t*)GC_MALLOC(sizeof(sl_string_t));
     obj->primitive_type = SL_T_STRING;
     return obj;
 }
