d3d82685757dcfb2e97e5218cb98664bf7f692c2
Hailey Somerville <hailey@hailey.lol>
2012-07-16 15:46:09 +1000

    sapi can define output function

--- inc/vm.h
+++ inc/vm.h
@@ -39,6 +39,7 @@ typedef struct sl_vm {
     int initializing;
     sl_lib_t lib;
     struct sl_catch_frame* catch_stack;
+    void(*output)(struct sl_vm* ctx, char* buff, size_t len);
 }
 sl_vm_t;
 
--- sapi/cli/cli.c
+++ sapi/cli/cli.c
@@ -3,16 +3,30 @@
 #include <stdlib.h>
 #include <string.h>
 
-static void lex(sl_vm_t* vm, void* state)
+static void
+output(sl_vm_t* vm, char* buff, size_t len)
+{
+    (void)vm;
+    fwrite(buff, len, 1, stdout);
+}
+
+static void run(sl_vm_t* vm, void* state)
 {    
     size_t token_count;
     sl_token_t* tokens;
+    sl_node_base_t* ast;
     char* src = "one plus two is: <%= 1 + 2 %>";
+    uint8_t* filename = (uint8_t*)"-";
+    sl_eval_ctx_t ctx;
     (void)state;
-    tokens = sl_lex(vm, (uint8_t*)"-", (uint8_t*)src, strlen(src), &token_count);
+    tokens = sl_lex(vm, filename, (uint8_t*)src, strlen(src), &token_count);
+    ast = sl_parse(vm, tokens, token_count, filename);
+    ctx.vm = vm;
+    ctx.self = vm->lib.Object;
+    ast->eval(ast, &ctx);
 }
 
-static void syntax_error(sl_vm_t* vm, void* state, SLVAL error)
+static void on_error(sl_vm_t* vm, void* state, SLVAL error)
 {
     SLVAL error_str = sl_to_s(vm, error);
     sl_string_t* str = (sl_string_t*)sl_get_ptr(error_str);
@@ -27,6 +41,7 @@ int main()
     sl_vm_t* vm;
     sl_static_init();
     vm = sl_init();
-    sl_try(vm, lex, syntax_error, NULL);
+    vm->output = output;
+    sl_try(vm, run, on_error, NULL);
     return 0;
 }
