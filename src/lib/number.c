76e0e31e93585df210cb541997bde2e7e65ab9c6
Hailey Somerville <hailey@hailey.lol>
2012-07-16 15:45:13 +1000

    sl_number_parse()

--- src/lib/number.c
+++ src/lib/number.c
@@ -1,5 +1,20 @@
 #include "class.h"
 #include "value.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+SLVAL
+sl_number_parse(sl_vm_t* vm, uint8_t* str, size_t len)
+{
+    /* TODO handle bignum upgrade as well */
+    int n;
+    char* buff = alloca(len + 1);
+    memcpy(buff, str, len);
+    buff[len + 1] = 0;
+    sscanf(buff, "%d", &n);
+    return sl_make_int(vm, n);
+}
 
 void
 sl_init_number(sl_vm_t* vm)
