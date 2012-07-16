192d378b7c51810e1d94f599d19eb075225b50d1
Hailey Somerville <hailey@hailey.lol>
2012-07-16 16:37:42 +1000

    make cli run code from files

--- sapi/cli/cli.c
+++ sapi/cli/cli.c
@@ -1,7 +1,15 @@
 #include "slash.h"
+#include <gc.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+typedef struct {
+    uint8_t* src;
+    size_t len;
+    uint8_t* filename;
+}
+cli_state_t;
 
 static void
 output(sl_vm_t* vm, char* buff, size_t len)
@@ -10,23 +18,23 @@ output(sl_vm_t* vm, char* buff, size_t len)
     fwrite(buff, len, 1, stdout);
 }
 
-static void run(sl_vm_t* vm, void* state)
+static void
+run(sl_vm_t* vm, void* state)
 {    
     size_t token_count;
     sl_token_t* tokens;
     sl_node_base_t* ast;
-    char* src = "one plus two is: <%= 1 + 2 %>";
-    uint8_t* filename = (uint8_t*)"-";
+    cli_state_t* st = state;
     sl_eval_ctx_t ctx;
-    (void)state;
-    tokens = sl_lex(vm, filename, (uint8_t*)src, strlen(src), &token_count);
-    ast = sl_parse(vm, tokens, token_count, filename);
+    tokens = sl_lex(vm, st->filename, st->src, st->len, &token_count);
+    ast = sl_parse(vm, tokens, token_count, st->filename);
     ctx.vm = vm;
     ctx.self = vm->lib.Object;
     ast->eval(ast, &ctx);
 }
 
-static void on_error(sl_vm_t* vm, void* state, SLVAL error)
+static void
+on_error(sl_vm_t* vm, void* state, SLVAL error)
 {
     SLVAL error_str = sl_to_s(vm, error);
     sl_string_t* str = (sl_string_t*)sl_get_ptr(error_str);
@@ -36,12 +44,44 @@ static void on_error(sl_vm_t* vm, void* state, SLVAL error)
     exit(1);
 }
 
-int main()
+static uint8_t*
+read_all(FILE* f, size_t* length)
 {
+    size_t len = 0;
+    size_t cap = 4096;
+    uint8_t* src = GC_MALLOC_ATOMIC(cap);
+    while(!feof(f)) {
+        if(len + 4096 > cap) {
+            cap *= 2;
+            src = GC_REALLOC(src, cap);
+        }
+        len += fread(src + len, 1, 4096, f);
+    }
+    *length = len;
+    return src;
+}
+
+int
+main(int argc, char** argv)
+{
+    cli_state_t state;
+    FILE* f;
     sl_vm_t* vm;
     sl_static_init();
     vm = sl_init();
     vm->output = output;
-    sl_try(vm, run, on_error, NULL);
+    if(argc > 1) {
+        state.filename = (uint8_t*)argv[1];
+        f = fopen((char*)state.filename, "rb");
+        if(!f) {
+            fprintf(stderr, "Can't open '%s' for reading\n", state.filename);
+            exit(1);
+        }
+    } else {    
+        state.filename = (uint8_t*)"(stdin)";
+        f = stdin;
+    }
+    state.src = read_all(f, &state.len);
+    sl_try(vm, run, on_error, &state);
     return 0;
 }
