ce37fb42662a945bd0a8342795a19ea374198eb7
Hailey Somerville <hailey@hailey.lol>
2012-07-19 21:14:01 +1000

    IO, puts() global function

---
+++ src/lib/io.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "slash.h"
+
+static SLVAL
+io_puts(sl_vm_t* vm, SLVAL self, size_t argc, SLVAL* argv)
+{
+    size_t i;
+    sl_string_t* str;
+    (void)self;
+    for(i = 0; i < argc; i++) {
+        str = (sl_string_t*)sl_get_ptr(sl_to_s(vm, argv[i]));
+        vm->output(vm, (char*)str->buff, str->buff_len);
+    }
+    return vm->lib.nil;
+}
+
+void
+sl_init_io(sl_vm_t* vm)
+{
+    sl_define_method(vm, vm->lib.Object, "puts", -1, io_puts);
+}
