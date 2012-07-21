1fcd25888c6b6876c29e01f05445d207f994b86b
Charlie Somerville <charlie@charliesomerville.com>
2012-07-21 23:13:51 +1000

    use absolute dir

--- .travis.sh
+++ .travis.sh
@@ -1,12 +1,15 @@
 #echo "Downloading gc-7.2.tar.gz"
 #wget http://www.hpl.hp.com/personal/Hans_Boehm/gc/gc_source/gc-7.2.tar.gz
 mkdir usr
+cd usr
+PREFIX="$(pwd)"
+cd ..
 echo "Downloading gmp-5.0.5.tar.bz"
 wget ftp://ftp.gnu.org/gnu/gmp/gmp-5.0.5.tar.bz2
 bzip2 -d gmp-5.0.5.tar.bz2
 tar xf gmp-5.0.5.tar
 cd gmp-5.0.5
 echo "Configuring gmp-5.0.5"
-./configure --prefix=../usr
+./configure --prefix=$PREFIX
 echo "Making gmp-5.0.5"
 make -j 8
