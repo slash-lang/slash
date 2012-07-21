46efeff378990b81d237d1fb3d1b1e78f118733f
Hailey Somerville <hailey@hailey.lol>
2012-07-21 17:41:23 +1000

    require, platform utilities

--- Makefile
+++ Makefile
@@ -2,11 +2,16 @@ CFLAGS=-Wall -Wextra -pedantic -Werror -ansi -Ideps/gc-7.2/include \
 	-iquote ./inc -g -Ideps/gmp-5.0.5 -fPIC
 LDFLAGS=
 
+PLATFORM=posix
+
 OBJS=src/class.o src/error.o src/method.o src/object.o src/st.o src/string.o \
 	src/value.o src/vm.o src/lib/int.o src/lib/number.o src/lib/float.o \
 	src/lib/bignum.o src/utf8.o src/lex.o src/lex_helper.o src/lib/nil.o \
 	src/lib/true.o src/lib/false.o src/eval.o src/parse.o src/parse_helper.o \
-	src/eval.o src/lib/io.o src/lib/array.o src/lib/comparable.o
+	src/eval.o src/lib/io.o src/lib/array.o src/lib/comparable.o \
+	src/lib/require.o
+
+OBJS += platform/$(PLATFORM).o
 
 .PHONY=clean default test
 
@@ -50,6 +55,7 @@ src/lex.o: CFLAGS += -Wno-unused -Wno-unused-parameter -Wno-sign-compare
 clean:
 	rm -f src/*.o
 	rm -f src/lib/*.o
+	rm -f platform/*.o
 	rm -f libgmp.a
 	rm -f libgc.a
 	rm -f libslash.a
---
+++ src/lib/require.c
@@ -0,0 +1,46 @@
+#include "slash.h"
+#include "platform.h"
+#include <gc.h>
+#include <string.h>
+
+static SLVAL
+require(sl_vm_t* vm, SLVAL self, SLVAL file)
+{
+    SLVAL include_dirs = sl_class_get_const(vm, vm->lib.Object, "INC");
+    SLVAL err;
+    char *canon_path;
+    size_t i, len;
+    sl_string_t* spath;
+    sl_expect(vm, include_dirs, vm->lib.Array);
+    sl_expect(vm, file, vm->lib.String);
+    len = sl_get_int(sl_array_length(vm, include_dirs));
+    for(i = 0; i < len; i++) {
+        spath = (sl_string_t*)sl_get_ptr(
+            sl_string_concat(vm, sl_array_get(vm, include_dirs, i),
+                sl_string_concat(vm, sl_make_cstring(vm, "/"), file)));
+        if(memchr(spath->buff, 0, spath->buff_len)) {
+            /* path contains a NULL byte, ignore */
+            continue;
+        }
+        if(sl_file_exists((char*)spath->buff)) {
+            canon_path = sl_realpath((char*)spath->buff);
+            sl_do_file(vm, (uint8_t*)canon_path);
+            return vm->lib._true;
+        }
+    }
+    err = sl_make_cstring(vm, "Could not load '");
+    err = sl_string_concat(vm, err, file);
+    err = sl_string_concat(vm, err, sl_make_cstring(vm, "'"));
+    sl_throw(vm, sl_make_error2(vm, vm->lib.Error, err));
+    (void)self;
+    return vm->lib.nil;
+}
+
+void
+sl_init_require(sl_vm_t* vm)
+{
+    SLVAL inc[1];
+    inc[0] = sl_make_cstring(vm, ".");
+    sl_class_set_const(vm, vm->lib.Object, "INC", sl_make_array(vm, 1, inc));
+    sl_define_method(vm, vm->lib.Object, "require", 1, require);
+}
