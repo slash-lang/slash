25bfc7f22c4fe9113b3eb301d7aa7f031d9a7019
Charlie Somerville <charlie@charliesomerville.com>
2012-07-22 14:36:00 +1000

    dumbest deprecation ever

--- platform/linux.c
+++ platform/linux.c
@@ -10,7 +10,6 @@ sl_stack_limit()
     void* addr;
     size_t size;
     pthread_getattr_np(self, &attr);
-    pthread_attr_getstackaddr(&attr, &addr);
-    pthread_attr_getstacksize(&attr, &size);
+    pthread_attr_getstack(&attr, &addr, &size);
     return (void*)((size_t)addr - size + 65536);
 }
