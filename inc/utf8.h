0de44adb5ca19cc0ab7fd62a87aa621a2a6caee4
Hailey Somerville <hailey@hailey.lol>
2012-08-01 18:11:16 +1000

    implement sl_utf8_each_char, and implement utf8 to utf32 function in terms of it

--- inc/utf8.h
+++ inc/utf8.h
@@ -10,6 +10,9 @@ sl_utf8_strlen(sl_vm_t* vm, uint8_t* buff, size_t len);
 uint32_t*
 sl_utf8_to_utf32(sl_vm_t* vm, uint8_t* buff, size_t len, size_t* out_len);
 
+uint32_t
+sl_utf8_each_char(sl_vm_t* vm, uint8_t** buff_ptr, size_t* len);
+
 size_t
 sl_utf32_char_to_utf8(sl_vm_t* vm, uint32_t u32c, uint8_t* u8buff);
 
