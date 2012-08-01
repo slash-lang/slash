7f476604bd19bb52beb9f1c3472d5ed8929b78a1
Hailey Somerville <hailey@hailey.lol>
2012-08-01 22:42:11 +1000

    change function used in yajl configure script

--- ext/json/configure.pl
+++ ext/json/configure.pl
@@ -2,7 +2,6 @@ check_lib "yajl_s", <<C;
     #include <yajl/yajl_parse.h>
     
     int main() {
-        char buff[100];
-        yajl_parse_integer(buff, 123456789);
+        yajl_alloc(NULL, NULL, NULL);
     }
 C
