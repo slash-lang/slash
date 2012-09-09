f454c701adf951834db6e834e45dab752223dc52
Hailey Somerville <hailey@hailey.lol>
2012-09-09 12:19:39 +1000

    remove debugging code

--- inc/slash/mem.h
+++ inc/slash/mem.h
@@ -9,22 +9,11 @@ sl_gc_arena_t;
 sl_gc_arena_t*
 sl_make_gc_arena();
 
-/*#define SL_LOG_ALLOCS*/
-#ifdef SL_LOG_ALLOCS
-    #define SL_ALLOC_ADDITIONAL_ARGS , int line, const char* file
-    #define sl_alloc(arena,size) sl_alloc_((arena),(size),__LINE__,__FILE__)
-    #define sl_alloc_buffer(arena,size) sl_alloc_buffer_((arena),(size),__LINE__,__FILE__)
-#else
-    #define SL_ALLOC_ADDITIONAL_ARGS
-    #define sl_alloc(arena,size) sl_alloc_((arena),(size))
-    #define sl_alloc_buffer(arena,size) sl_alloc_buffer_((arena),(size))
-#endif
+void*
+sl_alloc(sl_gc_arena_t* arena, size_t size);
 
 void*
-sl_alloc_(sl_gc_arena_t* arena, size_t size SL_ALLOC_ADDITIONAL_ARGS);
-
-void*
-sl_alloc_buffer_(sl_gc_arena_t* arena, size_t size SL_ALLOC_ADDITIONAL_ARGS);
+sl_alloc_buffer(sl_gc_arena_t* arena, size_t size);
 
 void*
 sl_realloc(sl_gc_arena_t* arena, void* ptr, size_t new_size);
