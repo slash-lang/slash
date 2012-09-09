7057cb6f3d37e5b611404d28d4e54f23f6fba14f
Hailey Somerville <hailey@hailey.lol>
2012-09-09 12:48:00 +1000

    BoundMethod

--- src/method.c
+++ src/method.c
@@ -18,14 +18,37 @@ allocate_bound_method(sl_vm_t* vm)
     return bound_method;
 }
 
+static SLVAL
+method_call(sl_vm_t* vm, SLVAL method, size_t argc, SLVAL* argv)
+{
+    sl_method_t* methp = (sl_method_t*)sl_get_ptr(method);
+    if(!methp->initialized) {
+        sl_throw_message2(vm, vm->lib.TypeError, "Can't call uninitialized Method");
+    }
+    return sl_apply_method(vm, argv[0], methp, argc - 1, argv + 1);
+}
+
+static SLVAL
+bound_method_call(sl_vm_t* vm, SLVAL bmethod, size_t argc, SLVAL* argv)
+{
+    sl_bound_method_t* bmethp = (sl_bound_method_t*)sl_get_ptr(bmethod);
+    if(!bmethp->method.initialized) {
+        sl_throw_message2(vm, vm->lib.TypeError, "Can't call uninitialized BoundMethod");
+    }
+    return sl_apply_method(vm, bmethp->self, &bmethp->method, argc, argv);
+}
+
 void
 sl_init_method(sl_vm_t* vm)
 {
     vm->lib.Method = sl_define_class(vm, "Method", vm->lib.Object);
     sl_class_set_allocator(vm, vm->lib.Method, allocate_method);
+    sl_define_method(vm, vm->lib.Method, "bind", 1, sl_method_bind);
+    sl_define_method(vm, vm->lib.Method, "call", -2, method_call);
     
     vm->lib.BoundMethod = sl_define_class(vm, "BoundMethod", vm->lib.Method);
     sl_class_set_allocator(vm, vm->lib.BoundMethod, allocate_bound_method);
+    sl_define_method(vm, vm->lib.BoundMethod, "call", -1, bound_method_call);
 }
 
 SLVAL
@@ -38,6 +61,7 @@ sl_make_c_func(sl_vm_t* vm, SLVAL klass, SLVAL name, int arity, SLVAL(*c_func)()
     methp->arity = arity;
     methp->klass = sl_expect(vm, klass, vm->lib.Class);
     methp->as.c.func = c_func;
+    methp->initialized = 1;
     return method;
 }
 
@@ -52,5 +76,26 @@ sl_make_method(sl_vm_t* vm, SLVAL klass, SLVAL name, sl_vm_section_t* section, s
     methp->klass = sl_expect(vm, klass, vm->lib.Class);
     methp->as.sl.section = section;
     methp->as.sl.parent_ctx = parent_ctx;
+    methp->initialized = 1;
     return method;
+}
+
+SLVAL
+sl_method_bind(sl_vm_t* vm, SLVAL method, SLVAL receiver)
+{
+    sl_method_t* methp = (sl_method_t*)sl_get_ptr(method);
+    sl_bound_method_t* bmethp = (sl_bound_method_t*)sl_get_ptr(sl_allocate(vm, vm->lib.BoundMethod));
+    
+    if(!methp->initialized) {
+        sl_throw_message2(vm, vm->lib.TypeError, "Can't bind uninitialized Method");
+    }
+    
+    bmethp->method.name      = methp->name;
+    bmethp->method.klass     = methp->klass;
+    bmethp->method.is_c_func = methp->is_c_func;
+    bmethp->method.arity     = methp->arity;
+    bmethp->method.as        = methp->as;
+    
+    bmethp->self = sl_expect(vm, receiver, methp->klass);
+    return sl_make_ptr((sl_object_t*)bmethp);
 }
