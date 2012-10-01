398e646cd66c01461e74b27d44257fcf604e5f81
Charlie Somerville <charlie@charliesomerville.com>
2012-10-01 14:09:20 +1000

    use inline variable declarations in opcode definitions, remove 'temp' variables

--- src/vm_exec.c
+++ src/vm_exec.c
@@ -11,7 +11,7 @@ INSTRUCTION(SL_OP_RAW, {
 /*  0: ECHO
     1: <reg> */
 INSTRUCTION(SL_OP_ECHO, {
-    tmp = sl_to_s(vm, NEXT_REG());
+    SLVAL tmp = sl_to_s(vm, NEXT_REG());
     tmp = sl_string_html_escape(vm, tmp);
     sl_response_write(vm, tmp);
 });
@@ -19,7 +19,7 @@ INSTRUCTION(SL_OP_ECHO, {
 /*  0: ECHO_RAW
     1: <reg> */
 INSTRUCTION(SL_OP_ECHO_RAW, {
-    tmp = sl_to_s(vm, NEXT_REG());
+    SLVAL tmp = sl_to_s(vm, NEXT_REG());
     sl_response_write(vm, tmp);
 });
 
@@ -27,7 +27,7 @@ INSTRUCTION(SL_OP_ECHO_RAW, {
     1: <reg:src>
     2: <reg:dest> */
 INSTRUCTION(SL_OP_NOT, {
-    tmp = NEXT_REG();
+    SLVAL tmp = NEXT_REG();
     if(sl_is_truthy(tmp)) {
         NEXT_REG() = V_FALSE;
     } else {
@@ -39,7 +39,7 @@ INSTRUCTION(SL_OP_NOT, {
     1: <reg:src>
     2: <reg:dest> */
 INSTRUCTION(SL_OP_MOV, {
-    tmp = NEXT_REG();
+    SLVAL tmp = NEXT_REG();
     NEXT_REG() = tmp;
 });
 
@@ -48,55 +48,55 @@ INSTRUCTION(SL_OP_MOV, {
     2: <uint:register_index>
     3: <reg:local> */
 INSTRUCTION(SL_OP_GET_OUTER, {
-    tmp_uint = NEXT_UINT();
-    tmp_ctx = ctx;
-    while(tmp_uint--) {
-        tmp_ctx = tmp_ctx->parent;
+    size_t frame_count = NEXT_UINT();
+    sl_vm_exec_ctx_t* parent_ctx = ctx;
+    while(frame_count--) {
+        parent_ctx = parent_ctx->parent;
     }
-    tmp_uint = NEXT_UINT();
-    NEXT_REG() = tmp_ctx->registers[tmp_uint];
+    size_t register_index = NEXT_UINT();
+    NEXT_REG() = parent_ctx->registers[register_index];
 });
 
 /*  0: GET_GLOBAL
     1: <str:var_name>
     2: <reg:dest> */
 INSTRUCTION(SL_OP_GET_GLOBAL, {
-    tmp_str = NEXT_STR();
-    NEXT_REG() = sl_get_global2(vm, tmp_str);
+    sl_string_t* var_name = NEXT_STR();
+    NEXT_REG() = sl_get_global2(vm, var_name);
 });
 
 /*  0: GET_IVAR
     1: <imm:var_name>
     2: <reg:dst> */
 INSTRUCTION(SL_OP_GET_IVAR, {
-    tmp_str = NEXT_STR();
-    NEXT_REG() = sl_get_ivar(vm, ctx->self, tmp_str);
+    sl_string_t* var_name = NEXT_STR();
+    NEXT_REG() = sl_get_ivar(vm, ctx->self, var_name);
 });
 
 /*  0: GET_CVAR
     1: <str:var_name>
     2: <reg:dest> */
 INSTRUCTION(SL_OP_GET_CVAR, {
-    tmp_str = NEXT_STR();
-    NEXT_REG() = sl_get_cvar(vm, ctx->self, tmp_str);
+    sl_string_t* var_name = NEXT_STR();
+    NEXT_REG() = sl_get_cvar(vm, ctx->self, var_name);
 });
 
 /*  0: GET_CONST
     1: <str:const_name>
     2: <reg:dest> */
 INSTRUCTION(SL_OP_GET_CONST, {
-    tmp = NEXT_IMM();
-    tmp_ctx = ctx;
-    while(tmp_ctx) {
-        if(sl_class_has_const2(vm, tmp_ctx->self, tmp)) {
-            NEXT_REG() = sl_class_get_const2(vm, tmp_ctx->self, tmp);
+    SLVAL const_name = NEXT_IMM();
+    sl_vm_exec_ctx_t* lookup_ctx = ctx;
+    while(lookup_ctx) {
+        if(sl_class_has_const2(vm, lookup_ctx->self, const_name)) {
+            NEXT_REG() = sl_class_get_const2(vm, lookup_ctx->self, const_name);
             break;
         }
-        tmp_ctx = tmp_ctx->parent;
+        lookup_ctx = lookup_ctx->parent;
     }
-    if(!tmp_ctx) {
+    if(!lookup_ctx) {
         /* force exception */
-        sl_class_get_const2(vm, ctx->self, tmp);
+        sl_class_get_const2(vm, ctx->self, const_name);
     }
 });
 
@@ -105,9 +105,9 @@ INSTRUCTION(SL_OP_GET_CONST, {
     2: <str:const_name>
     3: <reg:dest> */
 INSTRUCTION(SL_OP_GET_OBJECT_CONST, {
-    tmp = NEXT_REG();
-    tmp_str = NEXT_STR();
-    NEXT_REG() = sl_class_get_const2(vm, tmp, sl_make_ptr((sl_object_t*)tmp_str));
+    SLVAL obj = NEXT_REG();
+    SLVAL const_name = NEXT_IMM();
+    NEXT_REG() = sl_class_get_const2(vm, obj, const_name);
 });
 
 /*  0: SET_OUTER
@@ -115,45 +115,45 @@ INSTRUCTION(SL_OP_GET_OBJECT_CONST, {
     2: <uint:register_index>
     3: <reg:value> */
 INSTRUCTION(SL_OP_SET_OUTER, {
-    tmp_uint = NEXT_UINT();
-    tmp_ctx = ctx;
-    while(tmp_uint--) {
-        tmp_ctx = tmp_ctx->parent;
+    size_t frame_count = NEXT_UINT();
+    sl_vm_exec_ctx_t* lookup_ctx = ctx;
+    while(frame_count--) {
+        lookup_ctx = lookup_ctx->parent;
     }
-    tmp_uint = NEXT_UINT();
-    tmp_ctx->registers[tmp_uint] = NEXT_REG();
+    size_t register_index = NEXT_UINT();
+    lookup_ctx->registers[register_index] = NEXT_REG();
 });
 
 /*  0: SET_GLOBAL
     1: <str:var_name>
     2: <reg:value> */
 INSTRUCTION(SL_OP_SET_GLOBAL, {
-    tmp_str = NEXT_STR();
-    sl_set_global2(vm, tmp_str, NEXT_REG());
+    sl_string_t* var_name = NEXT_STR();
+    sl_set_global2(vm, var_name, NEXT_REG());
 });
 
 /*  0: SET_IVAR
     1: <imm:var_name>
     2: <reg:value> */
 INSTRUCTION(SL_OP_SET_IVAR, {
-    tmp_str = NEXT_STR();
-    sl_set_ivar(vm, ctx->self, tmp_str, NEXT_REG());
+    sl_string_t* var_name = NEXT_STR();
+    sl_set_ivar(vm, ctx->self, var_name, NEXT_REG());
 });
 
 /*  0: SET_CVAR
     1: <str:var_name>
     2: <reg:value> */
 INSTRUCTION(SL_OP_SET_CVAR, {
-    tmp_str = NEXT_STR();
-    sl_set_cvar(vm, ctx->self, tmp_str, NEXT_REG());
+    sl_string_t* var_name = NEXT_STR();
+    sl_set_cvar(vm, ctx->self, var_name, NEXT_REG());
 });
 
 /*  0: SET_CONST
     1: <str:const_name>
     2: <reg:value> */
 INSTRUCTION(SL_OP_SET_CONST, {
-    tmp = NEXT_IMM();
-    sl_class_set_const2(vm, ctx->self, tmp, NEXT_REG());
+    SLVAL const_name = NEXT_IMM();
+    sl_class_set_const2(vm, ctx->self, const_name, NEXT_REG());
 });
 
 /*  0: SET_OBJECT_CONST
@@ -161,17 +161,17 @@ INSTRUCTION(SL_OP_SET_CONST, {
     2: <str:const_name>
     3: <reg:value> */
 INSTRUCTION(SL_OP_SET_OBJECT_CONST, {
-    tmp = NEXT_REG();
-    tmp_str = NEXT_STR();
-    sl_class_set_const2(vm, tmp, sl_make_ptr((sl_object_t*)tmp_str), NEXT_REG());
+    SLVAL obj = NEXT_REG();
+    SLVAL const_name = NEXT_IMM();
+    sl_class_set_const2(vm, obj, const_name, NEXT_REG());
 });
 
 /*  0: IMMEDIATE
     1: <imm:value>
     2: <reg:dest> */
 INSTRUCTION(SL_OP_IMMEDIATE, {
-    tmp = NEXT_IMM();
-    NEXT_REG() = tmp;
+    SLVAL imm = NEXT_IMM();
+    NEXT_REG() = imm;
 });
 
 /*  0: SEND
@@ -181,29 +181,29 @@ INSTRUCTION(SL_OP_IMMEDIATE, {
     4: <uint:arg_count>
     5: <reg:dest> */
 INSTRUCTION(SL_OP_SEND, {
-    tmp = NEXT_REG();
-    tmp_str = NEXT_STR();
-    tmp_p = &NEXT_REG();
-    tmp_uint = NEXT_UINT();
+    SLVAL receiver = NEXT_REG();
+    SLVAL id = NEXT_IMM();
+    SLVAL* argv = &NEXT_REG();
+    size_t argc = NEXT_UINT();
     /* @TODO: backtrace stuff */
-    NEXT_REG() = sl_send2(vm, tmp, sl_make_ptr((sl_object_t*)tmp_str), tmp_uint, tmp_p);
+    NEXT_REG() = sl_send2(vm, receiver, id, argc, argv);
 });
 
 /*  0: JUMP
     1: <uint:ip> */
 INSTRUCTION(SL_OP_JUMP, {
-    tmp_uint = NEXT_UINT();
-    ip = tmp_uint;
+    size_t new_ip = NEXT_UINT();
+    ip = new_ip;
 });
 
 /*  0: JUMP_IF
     1: <uint:ip>
     2: <reg:condition> */
 INSTRUCTION(SL_OP_JUMP_IF, {
-    tmp_uint = NEXT_UINT();
-    tmp = NEXT_REG();
-    if(sl_is_truthy(tmp)) {
-        ip = tmp_uint;
+    size_t branch_ip = NEXT_UINT();
+    SLVAL val = NEXT_REG();
+    if(sl_is_truthy(val)) {
+        ip = branch_ip;
     }
 });
 
@@ -211,10 +211,10 @@ INSTRUCTION(SL_OP_JUMP_IF, {
     1: <uint:ip>
     2: <reg:condition> */
 INSTRUCTION(SL_OP_JUMP_UNLESS, {
-    tmp_uint = NEXT_UINT();
-    tmp = NEXT_REG();
-    if(!sl_is_truthy(tmp)) {
-        ip = tmp_uint;
+    size_t branch_ip = NEXT_UINT();
+    SLVAL val = NEXT_REG();
+    if(!sl_is_truthy(val)) {
+        ip = branch_ip;
     }
 });
 
@@ -224,12 +224,10 @@ INSTRUCTION(SL_OP_JUMP_UNLESS, {
     3: <section:body>
     4: <reg:dest> */
 INSTRUCTION(SL_OP_CLASS, {
-    /* tmp: class name */
-    tmp = NEXT_IMM();
-    /* tmp2: base class */
-    tmp2 = NEXT_REG();
-    tmp_section = NEXT_SECTION();
-    NEXT_REG() = vm_helper_define_class(ctx, tmp, tmp2, tmp_section);
+    SLVAL class_name = NEXT_IMM();
+    SLVAL super_class = NEXT_REG();
+    sl_vm_section_t* body = NEXT_SECTION();
+    NEXT_REG() = vm_helper_define_class(ctx, class_name, super_class, body);
 });
 
 /*  0: DEFINE
@@ -237,9 +235,9 @@ INSTRUCTION(SL_OP_CLASS, {
     2: <section:body>
     3: <reg:dest> */
 INSTRUCTION(SL_OP_DEFINE, {
-    tmp = NEXT_IMM();
-    tmp_section = NEXT_SECTION();
-    NEXT_REG() = vm_helper_define_method(ctx, tmp, tmp_section);
+    SLVAL id = NEXT_IMM();
+    sl_vm_section_t* body = NEXT_SECTION();
+    NEXT_REG() = vm_helper_define_method(ctx, id, body);
 });
 
 /*  0: DEFINE
@@ -248,18 +246,18 @@ INSTRUCTION(SL_OP_DEFINE, {
     3: <section:body>
     4: <reg:dest> */
 INSTRUCTION(SL_OP_DEFINE_ON, {
-    tmp = NEXT_REG();
-    tmp2 = NEXT_IMM();
-    tmp_section = NEXT_SECTION();
-    NEXT_REG() = vm_helper_define_singleton_method(ctx, tmp, tmp2, tmp_section);
+    SLVAL obj = NEXT_REG();
+    SLVAL id = NEXT_IMM();
+    sl_vm_section_t* body = NEXT_SECTION();
+    NEXT_REG() = vm_helper_define_singleton_method(ctx, obj, id, body);
 });
 
 /*  0: LAMBDA
     1: <section:body>
     2: <reg:dest> */
 INSTRUCTION(SL_OP_LAMBDA, {
-    tmp = sl_make_lambda(NEXT_SECTION(), ctx);
-    NEXT_REG() = tmp;
+    SLVAL lambda = sl_make_lambda(NEXT_SECTION(), ctx);
+    NEXT_REG() = lambda;
 });
 
 /*  0: SELF
@@ -273,9 +271,9 @@ INSTRUCTION(SL_OP_SELF, {
     2: <reg:first_element>
     3: <reg:dest> */
 INSTRUCTION(SL_OP_ARRAY, {
-    tmp_uint = NEXT_UINT();
-    tmp_p = &NEXT_REG();
-    NEXT_REG() = sl_make_array(vm, tmp_uint, tmp_p);
+    size_t count = NEXT_UINT();
+    SLVAL* elements = &NEXT_REG();
+    NEXT_REG() = sl_make_array(vm, count, elements);
 });
 
 /*  0: ARRAY_DUMP
@@ -283,19 +281,19 @@ INSTRUCTION(SL_OP_ARRAY, {
     2: <uint:count>
     3: <reg:first_element> */
 INSTRUCTION(SL_OP_ARRAY_DUMP, {
-    tmp = NEXT_REG();
-    tmp_uint = NEXT_UINT();
-    tmp_p = &NEXT_REG();
-    if(sl_is_a(vm, tmp, vm->lib.Array)) {
-        for(j = 0; j < tmp_uint; j++) {
-            tmp_p[j] = sl_array_get(vm, tmp, j);
+    SLVAL ary = NEXT_REG();
+    size_t count = NEXT_UINT();
+    SLVAL* registers = &NEXT_REG();
+    if(sl_is_a(vm, ary, vm->lib.Array)) {
+        for(size_t j = 0; j < count; j++) {
+            registers[j] = sl_array_get(vm, ary, j);
         }
     } else {
-        if(tmp_uint) {
-            tmp_p[0] = tmp;
+        if(count) {
+            registers[0] = ary;
         }
-        for(j = 1; j < tmp_uint; j++) {
-            tmp_p[j] = V_NIL;
+        for(size_t j = 1; j < count; j++) {
+            registers[j] = V_NIL;
         }
     }
 });
@@ -305,9 +303,9 @@ INSTRUCTION(SL_OP_ARRAY_DUMP, {
     2: <reg:first_element>
     3: <reg:dest> */
 INSTRUCTION(SL_OP_DICT, {
-    tmp_uint = NEXT_UINT();
-    tmp_p = &NEXT_REG();
-    NEXT_REG() = sl_make_dict(vm, tmp_uint, tmp_p);
+    size_t count = NEXT_UINT();
+    SLVAL* key_values = &NEXT_REG();
+    NEXT_REG() = sl_make_dict(vm, count, key_values);
 });
 
 /*  0: RETURN
@@ -322,9 +320,9 @@ INSTRUCTION(SL_OP_RETURN, {
     2: <reg:to>
     3: <reg:dest> */
 INSTRUCTION(SL_OP_RANGE, {
-    tmp = NEXT_REG();
-    tmp = sl_make_range(vm, tmp, NEXT_REG());
-    NEXT_REG() = tmp;
+    SLVAL left = NEXT_REG();
+    SLVAL right = NEXT_REG();
+    NEXT_REG() = sl_make_range(vm, left, right);
 });
 
 /*  0: RANGE_EX
@@ -332,9 +330,9 @@ INSTRUCTION(SL_OP_RANGE, {
     2: <reg:to>
     3: <reg:dest> */
 INSTRUCTION(SL_OP_RANGE_EX, {
-    tmp = NEXT_REG();
-    tmp = sl_make_range_exclusive(vm, tmp, NEXT_REG());
-    NEXT_REG() = tmp;
+    SLVAL left = NEXT_REG();
+    SLVAL right = NEXT_REG();
+    NEXT_REG() = sl_make_range_exclusive(vm, left, right);
 });
 
 /*  0: LINE
