b4e7e8dafd6f801dedfe6f06c66d2b2dc9875ae1
Charlie Somerville <charlie@charliesomerville.com>
2012-10-02 17:55:14 +1000

    use hard 4mb stack limit on linux

--- platform/linux.c
+++ platform/linux.c
@@ -2,6 +2,13 @@
 #include <pthread.h>
 #include <slash/platform.h>
 
+/*
+ * On Linux, the pthread functions sometimes give an unreasonable size for the
+ * stack. We'll set a hardcoded limit of 4 MB for the Slash stack and figure
+ * out where the top of the stack is with the pthread functions.
+ */
+#define STACK_SIZE (4*1024*1024)
+
 void*
 sl_stack_limit()
 {
@@ -12,5 +19,6 @@ sl_stack_limit()
     pthread_getattr_np(self, &attr);
     pthread_attr_getstack(&attr, &addr, &size);
     pthread_attr_destroy(&attr);
-    return (void*)((size_t)addr - size + 65536);
+    size_t stack_top = (size_t)addr + size;
+    return (void*)(stack_top - STACK_SIZE);
 }
