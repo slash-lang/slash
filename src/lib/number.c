9acd4e92296c0460aab6a28a67f505c538eccb42
Hailey Somerville <hailey@hailey.lol>
2012-10-04 13:23:50 +1000

    modify integer parsing routine to pass through mpz and allow decimal points

--- src/lib/number.c
+++ src/lib/number.c
@@ -10,17 +10,19 @@
 SLVAL
 sl_integer_parse(sl_vm_t* vm, uint8_t* str, size_t len)
 {
-    /* TODO handle bignum upgrade as well */
-    int n;
     mpz_t mpz;
     char* buff = sl_alloc(vm->arena, len + 1);
     memcpy(buff, str, len);
     buff[len] = 0;
+    char* dec = memchr(buff, '.', len);
+    if(dec) {
+        *dec = 0;
+    }
     mpz_init_set_str(mpz, buff, 10);
     if(mpz_cmp_si(mpz, INT_MIN / 2) > 0 && mpz_cmp_si(mpz, INT_MAX / 2) < 0) {
+        SLVAL retn = sl_make_int(vm, mpz_get_si(mpz));
         mpz_clear(mpz);
-        sscanf(buff, "%d", &n);
-        return sl_make_int(vm, n);
+        return retn;
     } else {
         mpz_clear(mpz);
         return sl_make_bignum_s(vm, buff);
