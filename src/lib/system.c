65dc072743f01e7d6b03e17f7ed6e4b515de48da
Hailey Somerville <hailey@hailey.lol>
2012-10-07 12:43:40 +1100

    remove strftime()

--- src/lib/system.c
+++ src/lib/system.c
@@ -18,33 +18,8 @@ object_exit(sl_vm_t* vm, SLVAL self, size_t argc, SLVAL* argv)
     return self; /* never reached */
 }
 
-static SLVAL
-object_strftime(sl_vm_t* vm, SLVAL self, SLVAL format, SLVAL time)
-{
-    time_t t;
-    struct tm* tm_ptr;
-    size_t capacity = 256;
-    char* fmt = sl_to_cstr(vm, format);
-    char* buff = sl_alloc_buffer(vm->arena, capacity);
-    if(sl_is_a(vm, time, vm->lib.Int)) {
-        t = sl_get_int(time);
-    } else if(sl_is_a(vm, time, vm->lib.Bignum)) {
-        t = sl_bignum_get_long(vm, time);
-    } else {
-        sl_expect(vm, time, vm->lib.Int);
-    }
-    tm_ptr = gmtime(&t);
-    while(strftime(buff, capacity, fmt, tm_ptr) == 0) {
-        capacity *= 2;
-        buff = sl_realloc(vm->arena, buff, capacity);
-    }
-    return sl_make_cstring(vm, buff);
-    (void)self; /* never reached */
-}
-
 void
 sl_init_system(sl_vm_t* vm)
 {
     sl_define_method(vm, vm->lib.Object, "exit", -1, object_exit);
-    sl_define_method(vm, vm->lib.Object, "strftime", 2, object_strftime);
 }
