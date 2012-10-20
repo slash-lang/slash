b54dce83d2804189f945d42f5c9613fada9b341b
Hailey Somerville <hailey@hailey.lol>
2012-10-20 23:02:41 +1100

    publicize regexp match functions

--- inc/slash/lib/regexp.h
+++ inc/slash/lib/regexp.h
@@ -7,4 +7,22 @@
 SLVAL
 sl_make_regexp(sl_vm_t* vm, uint8_t* re, size_t re_len, uint8_t* opts, size_t opts_len);
 
+SLVAL
+sl_regexp_match(sl_vm_t* vm, SLVAL self, size_t argc, SLVAL* argv);
+
+SLVAL
+sl_regexp_match_byte_offset(sl_vm_t* vm, SLVAL self, SLVAL i);
+
+SLVAL
+sl_regexp_match_index(sl_vm_t* vm, SLVAL self, SLVAL i);
+
+SLVAL
+sl_regexp_match_capture(sl_vm_t* vm, SLVAL self, SLVAL i);
+
+SLVAL
+sl_regexp_match_before(sl_vm_t* vm, SLVAL self);
+
+SLVAL
+sl_regexp_match_after(sl_vm_t* vm, SLVAL self);
+
 #endif
