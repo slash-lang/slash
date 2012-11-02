58a2e71455dffdbc90f7e6fbd30522f0762133ad
Charlie Somerville <charlie@charliesomerville.com>
2012-11-02 19:41:58 +1100

    adjust gc frequency

--- src/gc.c
+++ src/gc.c
@@ -6,7 +6,7 @@
 #include <stdint.h>
 
 #define POINTER_ALIGN_BYTES (4)
-#define ALLOCS_PER_GC_RUN (10000)
+#define ALLOCS_PER_GC_RUN (100000)
 #define ALLOC_BUCKET_COUNT (65536)
 
 typedef struct sl_gc_alloc {
