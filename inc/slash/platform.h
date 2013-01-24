911eb4d7f064ac7017882b0f25c9960d400dbbe5
Hailey Somerville <hailey@hailey.lol>
2013-01-24 22:47:21 +1100

    add sl_likely and sl_unlikely macro

--- inc/slash/platform.h
+++ inc/slash/platform.h
@@ -20,6 +20,14 @@ sl_seed();
 void*
 sl_stack_limit();
 
+#ifdef __GNUC__
+    #define sl_likely(x) __builtin_expect((x), 1)
+    #define sl_unlikely(x) __builtin_expect((x), 0)
+#else
+    #define sl_likely(x) (x)
+    #define sl_unlikely(x) (x)
+#endif
+
 #ifdef __APPLE__
     /* Mac OS X's setjmp() and longjmp() save the signal mask which is SLOOOOW.
        The _setjmp() and _longjmp() alternatives do not. */
