dd5d2ad79bb6a6395abec65bf522918a13ece810
Hailey Somerville <hailey@hailey.lol>
2013-09-29 15:53:28 +1000

    fix ext/posix to work with new style vm store interface

--- ext/posix/posix.c
+++ ext/posix/posix.c
@@ -8,7 +8,7 @@ static SLVAL
 sl_new_oserror(sl_vm_t* vm, int errno)
 {
     char* description = strerror(errno);
-    SLVAL Posix_OSError = sl_vm_store_get(vm, &cPosix_OSError);
+    SLVAL Posix_OSError = vm->store[cPosix_OSError];
     SLVAL error = sl_make_error2(vm, Posix_OSError, sl_make_cstring(vm, description));
     sl_set_ivar(vm, error, sl_intern(vm, "errno"), sl_make_int(vm, errno));
     return error;
@@ -25,13 +25,19 @@ sl_posix_oserror_get_errno(sl_vm_t* vm, SLVAL self)
 #include "getpid.c.inc"
 
 void
+sl_static_init_ext_posix()
+{
+    cPosix_OSError = sl_vm_store_register_slot();
+}
+
+void
 sl_init_ext_posix(sl_vm_t* vm)
 {
     SLVAL Posix = sl_define_class(vm, "Posix", vm->lib.Object);
     SLVAL Posix_OSError = sl_define_class3(vm, sl_intern(vm, "OSError"), vm->lib.Error, Posix);
     sl_define_method(vm, Posix_OSError, "errno", 0, sl_posix_oserror_get_errno);
 
-    sl_vm_store_put(vm, &cPosix_OSError, Posix_OSError);
+    vm->store[cPosix_OSError] = Posix_OSError;
 
     sl_init_ext_posix_errno(vm, Posix);
     sl_init_ext_posix_fork(vm, Posix);
